8284a8e9c0ea37cef4589ab2b3ec94c330a5bc91
yiying305 <76896453+yiying305@users.noreply.github.com>
2024-01-13 23:30:22 +0800

    Update index.js

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,2 @@
 //export const BASE_API_URL = "http://localhost:3001";
-export const BASE_API_URL = "https://9438-60-54-194-75.ngrok-free.app";
+export const BASE_API_URL = "https://0f5e-60-54-194-75.ngrok-free.app";
